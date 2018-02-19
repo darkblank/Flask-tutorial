eebe188ad296a1d057575a0cd17589f8dc7142c6
darkblank <darkblank1990@gmail.com>
2018-02-19 23:13:46 +0900

    파일위치 변경

--- tutorial/flaskr.py
+++ tutorial/flaskr.py
@@ -1,5 +1,6 @@
 # all the imports
 import sqlite3
+from contextlib import closing
 
 from flask import Flask
 
@@ -17,6 +18,13 @@ app.config.from_object(__name__)
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql') as f:
+            db.cursor().executescript(f.read().decode('utf-8'))
+        db.commit()
 
 
 if __name__ == '__main__':
