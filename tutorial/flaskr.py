3801f0cff5a4dfbc2fa49ed891d76b83ae1ff9f5
darkblank <darkblank1990@gmail.com>
2018-02-19 23:53:23 +0900

    최신 문서에 나와있는 설정으로 변경

--- tutorial/flaskr.py
+++ tutorial/flaskr.py
@@ -1,6 +1,5 @@
 # all the imports
-import sqlite3
-from contextlib import closing
+import os
 
 from flask import Flask
 
@@ -15,17 +14,12 @@ PASSWORD = 'default'
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
-def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
-
-
-def init_db():
-    with closing(connect_db()) as db:
-        with app.open_resource('schema.sql') as f:
-            db.cursor().executescript(f.read().decode('utf-8'))
-        db.commit()
-
-
-if __name__ == '__main__':
-    app.run()
+print(app.config)
